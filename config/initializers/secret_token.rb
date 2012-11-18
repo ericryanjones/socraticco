a857c2e9b438b02b7ef4bcd80cd11891c675d2fb
Eric Ryan Jones <eric@PaperlessTalent.com>
2012-11-18 00:31:58 -0500

    initial commit of Ruby on Rails framework and install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socraticco::Application.config.secret_token = '347b1620edf6ebb003705f0962a800e071a9c175538acc61dddc15564097dbf38f9e1f35d12b7317b35216790fd617ed93b8d1679147122771c3cafd07292264'
